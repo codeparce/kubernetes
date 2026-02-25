aa4118fe8b2893eecabc6b437bdabe1af39855e3
code parce <jhoandorado25@gmail.com>
2026-02-25 17:06:40 -0500

    DEVOPS : NEW : Servicios observabilidad y monitoreo

--- devops/dashboard/deploy_helm.sh
+++ devops/dashboard/deploy_helm.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 set -e
 
+pwd
+
 source .env
 
 # 1️⃣ Instalar el dashboard de Kubernetes
-helm repo add headlamp $REPO_HELP
+helm repo add $NAME_SPACE $REPO_HELP
+
 helm repo update
 
 kubectl create namespace $NAME_SPACE
-helm install $NAME_SPACE $PATH --namespace $NAME_SPACE
 
-helm upgrade --install headlamp ./
+helm install $NAME_SPACE $PATH_HELP --namespace $NAME_SPACE
 
-#kubectl create token headlamp --namespace headlamp > admin.txt
+kubectl apply -f ./igress.yaml
+
+#helm upgrade --install $NAME_SPACE ./chart
+
+#kubectl create token headlamp --namespace headlamp 
 
 #kubectl -n argocd get secret argocd-initial-admin-secret  -o jsonpath="{.data.password}" | base64 -d && echo
 
-#kubectl get secret monitoring-grafana -n monitoring \ -o jsonpath="{.data.admin-password}" | base64 -d && echo
+# kubectl --namespace monitoring get secrets monitoring-grafana -o jsonpath="{.data.admin-password}" | base64 -d ; echo
