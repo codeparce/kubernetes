c0b6c87d0b9a23fca0430b75c32ac35c68ee9ecf
code parce <jhoandorado25@gmail.com>
2026-02-26 17:45:06 -0500

    DEVOPS : FIX : Correcion de scripts

--- devops/dashboard/deploy_helm.sh
+++ devops/dashboard/deploy_helm.sh
@@ -14,12 +14,14 @@ kubectl create namespace $NAME_SPACE
 
 helm install $NAME_SPACE $PATH_HELP --namespace $NAME_SPACE
 
+envsubst < igress.yaml.template > igress.yaml
+
 kubectl apply -f ./igress.yaml
 
 #helm upgrade --install $NAME_SPACE ./chart
 
-#kubectl create token headlamp --namespace headlamp 
+# kubectl create token headlamp --namespace headlamp 
 
-#kubectl -n argocd get secret argocd-initial-admin-secret  -o jsonpath="{.data.password}" | base64 -d && echo
+# kubectl -n argocd get secret argocd-initial-admin-secret  -o jsonpath="{.data.password}" | base64 -d && echo
 
 # kubectl --namespace monitoring get secrets monitoring-grafana -o jsonpath="{.data.admin-password}" | base64 -d ; echo
